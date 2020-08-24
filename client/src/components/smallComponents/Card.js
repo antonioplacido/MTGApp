bff40826543c4cb8ba736a042d2be5a8bae969a8
Antonio Placido <aplacido1992@gmail.com>
2020-08-24 15:43:59 -0400

    have decent css, cards displaying nicely, conditional rendering omits cards without image uris

--- client/src/components/smallComponents/Card.js
+++ client/src/components/smallComponents/Card.js
@@ -1,27 +1,27 @@
 import React from "react";
 import styled from "styled-components";
 
-export default function Card({ cards, imageUri, price, name }) {
+export default function Card({ cards, name }) {
   console.log(name);
   console.log(cards);
   return (
     <Wrapper>
-      <h1>{name}</h1>
-      <p>Price</p>
-
       {cards.map((c) => {
-        console.log(c);
         return (
           <>
-            {c.image_uris && <img src={c.image_uris.small} alt="" />}
-            <div key={c}>
-              {c.name}
-              <Buttons>
-                <WishlistAdd>Wishlist</WishlistAdd>
-                <TradeAdd>Trade</TradeAdd>
-                <DeckAdd>Deck</DeckAdd>
-              </Buttons>
-            </div>
+            <CardWrapper>
+              {c.image_uris && <img src={c.image_uris.normal} alt="" />}
+              {c.image_uris && (
+                <div key={c}>
+                  <Buttons>
+                    <WishlistAdd>Wishlist</WishlistAdd>
+                    <TradeAdd>Trade</TradeAdd>
+                    <DeckAdd>Deck</DeckAdd>
+                    <CollectionAdd>Collection</CollectionAdd>
+                  </Buttons>
+                </div>
+              )}
+            </CardWrapper>
           </>
         );
       })}
@@ -29,22 +29,31 @@ export default function Card({ cards, imageUri, price, name }) {
   );
 }
 
+const CardWrapper = styled.div`
+  display: inline-block;
+  padding: 15px;
+  img {
+    height: 400px;
+  }
+`;
+
+const CollectionAdd = styled.button``;
+
 const WishlistAdd = styled.button``;
 
 const TradeAdd = styled.button``;
 
 const DeckAdd = styled.button``;
 
-const Buttons = styled.div``;
-
-const Imagediv = styled.img`
-  height: 100px;
-  width: 100px;
+const Buttons = styled.div`
+  display: flex;
+  justify-content: space-evenly;
 `;
 
 const Wrapper = styled.div`
   position: relative;
-  display: flex;
+  display: inline-block;
+  padding-top: 50px;
   align-items: center;
   height: 350px;
 `;
